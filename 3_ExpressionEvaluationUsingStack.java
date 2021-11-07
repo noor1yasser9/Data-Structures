935e3c7847c94e629de5bdda4a91e90be169630e
Noor Yasser <41232970+noor1yasser9@users.noreply.github.com>
2021-11-07 13:03:49 +0200

    Disadvantages infix  Advantage postfix or prefix
      Disadvantages infix 
            -use parentheses   
            -need precedence 
    
           Advantage postfix or prefix  
            + Precedence has been conidered

--- 3_ExpressionEvaluationUsingStack.java
+++ 3_ExpressionEvaluationUsingStack.java
@@ -1,6 +1,12 @@
 // Expression Evaluation Using Stack 
     // infix
       //1*5*(5+9)/8
+    /*
+        Disadvantages infix 
+        -use parentheses   
+        -need precedence 
+    */
+
     // postfix => suffix or reverse polish notation
         //1*5*(5 9 +)/8
            //1 5 * *(5 9 +)/8
@@ -28,3 +34,10 @@
         //( * A B  ) + ( / C D  )
         // * A B  + / C D 
         //  + * A B / C D 
+        
+    /*
+        Advantage postfix or prefix  
+        + Precedence has been conidered 
+    */
+
+
