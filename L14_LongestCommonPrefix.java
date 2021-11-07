2c79f6602490de1b7c26974e892c55474f199834
Noor Yasser <41232970+noor1yasser9@users.noreply.github.com>
2021-11-07 17:39:28 +0200

    Update L14_LongestCommonPrefix.java

--- L14_LongestCommonPrefix.java
+++ L14_LongestCommonPrefix.java
@@ -18,6 +18,8 @@
   // 0 <= strs[i].length <= 200
   // strs[i] consists of only lower-case English letters.
 
+// Question Link
+// https://leetcode.com/problems/longest-common-prefix/
 
 class Solution {
     public String longestCommonPrefix(String[] strs) {
