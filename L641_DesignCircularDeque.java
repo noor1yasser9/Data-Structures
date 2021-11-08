3238a079cdadedd78b1e57bd27056952981af8f2
Noor Yasser <41232970+noor1yasser9@users.noreply.github.com>
2021-11-08 19:33:22 +0200

    Design your implementation of the circular double-ended queue (deque).

--- L641_DesignCircularDeque.java
+++ L641_DesignCircularDeque.java
@@ -34,3 +34,96 @@
   // 1 <= k <= 1000
   // 0 <= value <= 1000
   // At most 2000 calls will be made to insertFront, insertLast, deleteFront, deleteLast, getFront, getRear, isEmpty, isFull.
+
+
+class MyCircularDeque {
+    int rear =0;
+	int front;
+	int length = 0;
+	int maxSize;
+	int[] arr;
+    
+    public MyCircularDeque(int k) {
+        maxSize  = k;
+	    arr= new int[k];
+	    front =  k - 1;
+    }
+    
+    public boolean insertFront(int value) {
+       if (!isFull()){
+	        arr[front] = value;
+            front = (front - 1 + arr.length) % arr.length;
+	        length++;
+            return true;
+	    }
+        System.out.println("Queue Full Can't Enqueue ...!");
+	     return false; 
+    }
+    
+    public boolean insertLast(int value) {
+        if (!isFull()){
+	        arr[rear] = value;
+            rear = (rear + 1) % arr.length;
+	        length++;
+            return true;
+	    }
+        System.out.println("Queue Full Can't Enqueue ...!");
+	     return false;
+    }
+    
+    public boolean deleteFront() {
+         if (!isEmpty()){
+             front = (this.front + 1) % arr.length;
+             --length;
+          return true;
+	    }
+	        	
+        System.out.println("Empty Queue Can't Dequeue ...!");
+        return false;
+    }
+    
+    public boolean deleteLast() {
+      if (!isEmpty()){
+	        rear = (rear - 1 + arr.length) % arr.length;
+			length--;
+          return true;
+	    }
+	        	
+        System.out.println("Empty Queue Can't Dequeue ...!");
+        return false;
+	     
+    }
+    
+    public int getFront() {
+        if(isEmpty()) return -1; 
+		return arr[(front + 1) % arr.length];
+    }
+    
+    public int getRear() {
+          if(isEmpty()) return -1; 
+		return arr[(rear - 1 + arr.length) % arr.length]; 
+    }
+    
+    public boolean isEmpty() {
+       return length == 0; 
+    }
+    
+    public boolean isFull() {
+        return length == maxSize;
+    }
+}
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque obj = new MyCircularDeque(k);
+ * boolean param_1 = obj.insertFront(value);
+ * boolean param_2 = obj.insertLast(value);
+ * boolean param_3 = obj.deleteFront();
+ * boolean param_4 = obj.deleteLast();
+ * int param_5 = obj.getFront();
+ * int param_6 = obj.getRear();
+ * boolean param_7 = obj.isEmpty();
+ * boolean param_8 = obj.isFull();
+ */
+
+
