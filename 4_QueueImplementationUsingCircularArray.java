2eee637dc05732ce4988f98d9dd6fc24f57c0a19
Noor Yasser <41232970+noor1yasser9@users.noreply.github.com>
2021-11-08 17:44:35 +0200

    Implementation of Deque using circular array
    // Implementation of Deque using circular array
    //Circular array implementation deque 
    // For implementing deque, we need to keep track of two indices, front and rear. 
    // We enqueue(push) an item at the rear or the front end of qedue and dequeue(pop) an item from both rear and front end.

--- 4_QueueImplementationUsingCircularArray.java
+++ 4_QueueImplementationUsingCircularArray.java
@@ -1,1 +1,5 @@
 
+// Implementation of Deque using circular array
+//Circular array implementation deque 
+// For implementing deque, we need to keep track of two indices, front and rear. 
+// We enqueue(push) an item at the rear or the front end of qedue and dequeue(pop) an item from both rear and front end.
