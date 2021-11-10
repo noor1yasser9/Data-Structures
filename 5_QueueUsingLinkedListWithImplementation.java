87ff63ce05316ef0d605edcf6c6a41ecbf42182c
Noor Yasser <41232970+noor1yasser9@users.noreply.github.com>
2021-11-10 18:46:39 +0200

    Create Class LinkedQueue
    finish function check list isEmpty()
    finish function get front()
    finish function getRear()
    finish function enqueue()
    finish function dequeue()
    finish function clearQueue()
    finish function display()
    finish function getSize()

--- 5_QueueUsingLinkedListWithImplementation.java
+++ 5_QueueUsingLinkedListWithImplementation.java
@@ -10,3 +10,65 @@ class Node<T> {
         this.item = item;
     }
 }
+
+class LinkedQueue<T> {
+
+    private Node<T> next = null,
+            frontPtr = null,
+            rearPtr = null;
+    private int length = 0;
+    
+     boolean isEmpty() {
+        return length == 0;
+    }
+    
+    T front() {
+        assert !isEmpty();
+        return frontPtr.item;
+    }
+
+    T rear() {
+        assert !isEmpty();
+        return rearPtr.item;
+    }
+    
+    void enqueue(T item) {
+        Node<T> node = new Node<T>(item);
+        if (length == 0) {
+            this.frontPtr = this.rearPtr = node;
+        }
+        this.rearPtr.next = node;
+        this.rearPtr = node;
+        length++;
+    }
+    
+    void dequeue() {
+        if (isEmpty()) {
+            return;
+        }
+        this.frontPtr = this.frontPtr.next;
+        if (this.frontPtr == null) {
+            this.rearPtr = null;
+        }
+
+        length--;
+    }
+
+    void clearQueue() {
+        frontPtr = null;
+    }
+
+    void display() {
+        Node<T> cur = frontPtr;
+        System.out.print("[ ");
+        while (cur != null) {
+            System.out.print(cur.item + " ");
+            cur = cur.next;
+        }
+        System.out.println("]");
+    }
+
+    int getSize() {
+        return length;
+    }
+}
