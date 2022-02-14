5caafb795149d0a41ff1efc4f50d549f0295fee1
Noor Yasser <41232970+noor1yasser9@users.noreply.github.com>
2022-02-14 23:00:32 +0200

    Update L448_Find_All_Numbers_Disappeared_in_an_Array.java

--- L448_Find_All_Numbers_Disappeared_in_an_Array.java
+++ L448_Find_All_Numbers_Disappeared_in_an_Array.java
@@ -22,3 +22,27 @@ n == nums.length
 
 Follow up: Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
 */
+
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        
+         List<Integer> result = new ArrayList<>();
+        
+           for (int i = 0; i < nums.length; i++) {
+            int val = Math.abs(nums[i]) - 1;
+            if (nums[val] > 0) {
+                nums[val] = -nums[val];
+            }
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+        
+        return result;
+        
+    }
+}
